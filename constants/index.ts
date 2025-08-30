43182d853ea7f264ae855c78bf11f71e0321975e
finetunexyz <dhruvanshg7@gmail.com>
2025-08-30 16:18:01 +0530

    update vapi sdk and implement new way to handle workflows

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-import { CreateAssistantDTO } from "@vapi-ai/web/dist/api";
+import { CreateAssistantDTO, CreateWorkflowDTO } from "@vapi-ai/web/dist/api";
 import { z } from "zod";
 
 export const mappings = {
@@ -97,63 +97,170 @@ export const mappings = {
   "aws amplify": "amplify",
 };
 
-export const interviewer: CreateAssistantDTO = {
-  name: "Interviewer",
-  firstMessage:
-    "Hello! Thank you for taking the time to speak with me today. I'm excited to learn more about you and your experience.",
-  transcriber: {
-    provider: "deepgram",
-    model: "nova-2",
-    language: "en",
-  },
-  voice: {
-    provider: "11labs",
-    voiceId: "sarah",
-    stability: 0.4,
-    similarityBoost: 0.8,
-    speed: 0.9,
-    style: 0.5,
-    useSpeakerBoost: true,
-  },
-  model: {
-    provider: "openai",
-    model: "gpt-4",
-    messages: [
-      {
-        role: "system",
-        content: `You are a professional job interviewer conducting a real-time voice interview with a candidate. Your goal is to assess their qualifications, motivation, and fit for the role.
-
-Interview Guidelines:
-Follow the structured question flow:
-{{questions}}
-
-Engage naturally & react appropriately:
-Listen actively to responses and acknowledge them before moving forward.
-Ask brief follow-up questions if a response is vague or requires more detail.
-Keep the conversation flowing smoothly while maintaining control.
-Be professional, yet warm and welcoming:
-
-Use official yet friendly language.
-Keep responses concise and to the point (like in a real voice interview).
-Avoid robotic phrasing—sound natural and conversational.
-Answer the candidate’s questions professionally:
-
-If asked about the role, company, or expectations, provide a clear and relevant answer.
-If unsure, redirect the candidate to HR for more details.
-
-Conclude the interview properly:
-Thank the candidate for their time.
-Inform them that the company will reach out soon with feedback.
-End the conversation on a polite and positive note.
-
-
-- Be sure to be professional and polite.
-- Keep all your responses short and simple. Use official language, but be kind and welcoming.
-- This is a voice conversation, so keep your responses short, like in a real conversation. Don't ramble for too long.`,
+export const interviewer: CreateWorkflowDTO = 
+  {
+  "name": "ai_interview_prep",
+  "nodes": [
+    {
+      "name": "introduction",
+      "type": "conversation",
+      "isStart": true,
+      "metadata": {
+        "position": {
+          "x": -657.8897692862682,
+          "y": -106.29865816865248
+        }
       },
-    ],
-  },
-};
+      "prompt": "You are a friendly AI interview preparation assistant. Greet the user by name and ask for their interview requirements like {{role}}, interview {{type}} (technical/behavioral/mixed), experience{{level}}, {{techstack}}, and number of questions {{amount}}. Collect this information conversationally, then confirm all details before proceeding. Keep the conversation natural and encouraging.\nGreet the user. Inform them that you will get some information from them to create a perfect interview. Ask the caller for dthe ata required to extract. Ask the questions one by one, and await an answer.\n",
+      "globalNodePlan": {
+        "enabled": true,
+        "enterCondition": ""
+      },
+      "variableExtractionPlan": {
+        "output": []
+      },
+      "messagePlan": {
+        "firstMessage": "Hello, {{username}}! Let's prepare your interview. I'll ask you a few questions and generate a perfect interview just for you. Are you ready?"
+      },
+      "toolIds": [
+        "823748c8-2147-4e72-9422-4dc30996d758"
+      ]
+    },
+    {
+      "name": "apiRequest_1756363549561",
+      "type": "tool",
+      "metadata": {
+        "position": {
+          "x": -669.6653137801169,
+          "y": 911.6478619994798
+        }
+      },
+      "tool": {
+        "url": "https://ai-interview-prep-two-red.vercel.app/api/vapi/generate",
+        "body": {
+          "type": "object",
+          "required": [],
+          "properties": {
+            "role": {
+              "type": "string",
+              "default": "{{role}}",
+              "description": ""
+            },
+            "type": {
+              "type": "string",
+              "default": "{{type}}",
+              "description": ""
+            },
+            "level": {
+              "type": "string",
+              "default": "{{level}}",
+              "description": ""
+            },
+            "amount": {
+              "type": "string",
+              "default": "{{amount}}",
+              "description": ""
+            },
+            "userid": {
+              "type": "string",
+              "default": "{{userid}}",
+              "description": ""
+            },
+            "techstack": {
+              "type": "string",
+              "default": "{{techstack}}",
+              "description": ""
+            }
+          }
+        },
+        "type": "apiRequest",
+        "method": "POST",
+        "function": {
+          "name": "api_request_tool",
+          "parameters": {
+            "type": "object",
+            "required": [],
+            "properties": {}
+          },
+          "description": "API request tool"
+        },
+        "messages": [
+          {
+            "type": "request-start",
+            "content": "Please hold on. I am sending a request to the app.",
+            "blocking": true
+          },
+          {
+            "role": "assistant",
+            "type": "request-complete",
+            "content": "The request has been sent, and the interview has been generated. Thank you for your call. Bye!",
+            "endCallAfterSpokenEnabled": true
+          },
+          {
+            "type": "request-failed",
+            "content": "Oops, looks like something went wrong when sending the data to the app. Please try again",
+            "endCallAfterSpokenEnabled": true
+          }
+        ],
+        "variableExtractionPlan": {
+          "schema": {
+            "type": "object",
+            "required": [],
+            "properties": {}
+          },
+          "aliases": []
+        }
+      }
+    },
+    {
+      "name": "hangup_1756441452140",
+      "type": "tool",
+      "metadata": {
+        "position": {
+          "x": -669.2807296621218,
+          "y": 1315.6230956070249
+        }
+      },
+      "tool": {
+        "type": "endCall",
+        "function": {
+          "name": "untitled_tool",
+          "parameters": {
+            "type": "object",
+            "required": [],
+            "properties": {}
+          }
+        },
+        "messages": [
+          {
+            "type": "request-start",
+            "content": "Everything has been generated. I will redirect you to the dashboard now. Thanks for the call!",
+            "blocking": true
+          }
+        ]
+      }
+    }
+  ],
+  "edges": [
+    {
+      "from": "introduction",
+      "to": "apiRequest_1756363549561",
+      "condition": {
+        "type": "ai",
+        "prompt": "if the user provided all the data to be extra"
+      }
+    },
+    {
+      "from": "apiRequest_1756363549561",
+      "to": "hangup_1756441452140",
+      "condition": {
+        "type": "ai",
+        "prompt": ""
+      }
+    }
+  ],
+  "globalPrompt": ""
+}
 
 export const feedbackSchema = z.object({
   totalScore: z.number(),
