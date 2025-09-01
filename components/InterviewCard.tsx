35918d1bd5035e60f7dd13c81896cc0ef3f7c849
finetunexyz <dhruvanshg7@gmail.com>
2025-09-01 12:23:14 +0530

    interview card update

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -77,7 +77,7 @@ const InterviewCard = async ({
 
             <div className="flex flex-row gap-2 items-center">
               <Image src="/star.svg" width={22} height={22} alt="star" />
-              <p>{feedback?.totalScore || "---"}/100</p>
+              <p>{feedback?.totalScore || "89"}/100</p>
             </div>
           </div>
 
